959983f62568b6bf487f0ebc79cef81ff0a624c8
Stefan881 <ondeyostephen0@gmail.com>
2024-04-27 18:49:45 +0300

    linked all other pages

--- WiSSA/WiSSA_App/urls.py
+++ WiSSA/WiSSA_App/urls.py
@@ -3,7 +3,13 @@ from .import views
 
 
 urlpatterns = [
-    path('',views.index,name='index-url')
+    path('',views.index,name='index-url'),
+    path('about',views.about,name='about-url'),
+    path('contact',views.contact,name='contact-url'),
+    path('news',views.news,name='news-url'),
 ]
+
+
+
 
 
--- WiSSA/WiSSA_App/views.py
+++ WiSSA/WiSSA_App/views.py
@@ -5,6 +5,17 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'index.html',{})
 
+def about(request):
+    return render(request,'about.html',{})
+
+def contact(request):
+    return render(request,'contact.html')
+
+def news(request):
+    return render(request,'news.html',{})
+
+
+
 
 
 
