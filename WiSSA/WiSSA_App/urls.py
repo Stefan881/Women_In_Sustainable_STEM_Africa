d5d1408c93563593a8af0d244daba1dd5114b6cb
Stefan881 <ondeyostephen0@gmail.com>
2024-04-28 11:43:39 +0300

    integrated Gmail api

--- WiSSA/WiSSA_App/urls.py
+++ WiSSA/WiSSA_App/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('about',views.about,name='about-url'),
     path('contact',views.contact,name='contact-url'),
     path('news',views.news,name='news-url'),
+    path('support_us',views.support_us,name='support_us-url')
 ]
+
 
 
 
--- WiSSA/WiSSA_App/views.py
+++ WiSSA/WiSSA_App/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 # Create your views here.
 def index(request):
     return render(request,'index.html',{})
@@ -9,10 +10,28 @@ def about(request):
     return render(request,'about.html',{})
 
 def contact(request):
+    if request.method == 'POST':
+        message_name = request.POST['name']
+        message_email =  request.POST['email']
+        message = request.POST['message']
+
+        # function to send the email
+        send_mail(
+            message_name,
+            message,
+            message_email,
+            ['ondeyostephen0@gmail.com']
+        )
+        return render(request,'contact.html',{'message_name':message_name})
     return render(request,'contact.html')
 
 def news(request):
     return render(request,'news.html',{})
+
+def support_us(request):
+    return render(request,'support_us.html',{})
+
+
 
 
 
